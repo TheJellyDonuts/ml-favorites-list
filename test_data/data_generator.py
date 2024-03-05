fcd69f4386b1a6f372fb543b7a8469d1599ca6f1
kcouv <kcouvion@cedarville.edu>
2024-03-05 15:31:35 -0700

    updates sys.args to argparse

--- test_data/data_generator.py
+++ test_data/data_generator.py
@@ -1,13 +1,20 @@
-import sys, random, csv, pandas
+import random, csv, pandas, argparse
 from datetime import datetime
 from tqdm import tqdm
 
 # Initialize according to user-provided arguments
-numEntries = 100
-if len(sys.argv) == 2 and sys.argv[1].isnumeric():
-  numEntries = (int)(sys.argv[1])
-elif len(sys.argv) > 2:
-  sys.exit("Expexted arguments: 1\n <numEntries> (optional): Number of data rows to generate.")
+parser = argparse.ArgumentParser(
+  prog='Data Generator',
+  description='Generates random data to be analyzed by our various algorithms.',
+)
+parser.add_argument(
+  'num_entries',
+  type=int,
+  default=100,
+  help='The number of data entries to generate.',
+  nargs='?'
+)
+args = parser.parse_args()
 
 # Generate a list of dateTimes within a range
 random.seed()
@@ -15,7 +22,7 @@ dates = []
 start_date = datetime(2023, 1, 1, 00, 00, 00)
 end_date = datetime(2023, 12, 31, 23, 59, 59)
 
-for i in tqdm(range(numEntries), desc='Generating random dates'):
+for i in tqdm(range(args.num_entries), desc='Generating random dates'):
   random_date = start_date + (end_date - start_date) * random.random()
   formatted_date = datetime.strftime(random_date, '%Y-%m-%d %H:%M:%S')
   dates.append(formatted_date)
