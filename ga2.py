1b9f3d06b17ef2f105d46869978ee4b38604980d
theTrueEnder <67336179+theTrueEnder@users.noreply.github.com>
2024-03-05 11:41:29 -0700

    ga2 working

--- ga2.py
+++ ga2.py
@@ -3,7 +3,7 @@ from geneticalgorithm import geneticalgorithm as ga
 import json
 import re
 
-string_len = 25
+string_len = 250
 
 num_to_tool = {}
 with open('enum_tools.json', 'r') as f:
@@ -17,32 +17,40 @@ with open('day_tools.json', 'r') as f:
 
 tool_strings = []
 for value in day_tools.values():
-    if len(value) >= 10:
+    if len(value) >= 1:
         temp = []
         for val in value:
             temp.append(tool_to_num.get(val))
         tool_strings.append(temp)
     
-print(tool_strings[0:3])
 
 def f(tool_num_list):
+    joined_ary = np.array2string(np.asarray(tool_num_list, dtype=int))[1:-1]
+    joined_ary = joined_ary.replace(r' +', ' ')
     score = 0
-    joined_ary = " ".join(tool_num_list)
     for lst in tool_strings:
-        score += len(re.findall(joined_ary, ' '.join(lst)))
-    
+        lll = ' '.join(lst)
+        ll = re.findall(lll, joined_ary)
+        score += len(ll)
+
+    # if score != 0:
+    #     print(score)
     return 1/(score+1)
 
-z= tool_strings[0]
-print(f(z))
+def f2(joined_ary):
+    score = 0
+    for lst in tool_strings:
+        lll = ' '.join(lst)
+        ll = re.findall(lll, joined_ary)
+        score += len(ll)
+
+    return score
 
 
-x= 1/0
 
+varbound=np.array([[0,len(num_to_tool)-1]]*string_len)
 
-varbound=np.array([[0,len(num_to_tool)]]*string_len)
-
-algorithm_param = {'max_num_iteration': 1000,
+algorithm_param = {'max_num_iteration': 5000,
                    'population_size':100, # no touch
                    'mutation_probability':0.1,
                    'elit_ratio': 0.01,
@@ -63,5 +71,14 @@ model=ga(function=f,
 model.run()
 convergence=model.report
 solution=model.output_dict
-print(convergence)
-print(solution)
+# print(convergence)
+# print(solution)
+with open('result.txt', 'w') as f:
+    app_lst = [num_to_tool[str(int(app))] for app in solution['variable']]
+    s = np.array2string(solution['variable'], max_line_width=5000000)[1:-1].replace('.', '').replace('  ', ' ')
+    score = f2(s)
+    print('Score:', score, app_lst)
+    f.write(' '.join(app_lst))
+    f.write("\n")
+    f.write(s)
+    f.write(f"\nScore: {str(score)}")
