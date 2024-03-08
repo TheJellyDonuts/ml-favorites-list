e355c1a14ccfb5d21f6c6b23ef08b5e9f8ae4836
svenable1 <67489742+svenable1@users.noreply.github.com>
2024-03-07 17:08:35 -0700

    Made ratings csv for dataset

--- src/machine learning/RNN/test/rating_generator.py
+++ src/machine learning/RNN/test/rating_generator.py
@@ -8,11 +8,12 @@ cur_dir = os.path.dirname(__file__)
 data_dir = os.path.join(cur_dir, '../../../data')
 
 # Set File Path
-file_path = os.path.join(data_dir, "kardia_data_one_year.csv")
+file_path = os.path.join(data_dir, "kardia_data_five_years.csv")
 
 # Load the data
 data = pd.read_csv(file_path, header=0)
 
+# * Create a CSV file with Unique App Paths
 # Get all the unique app paths
 unique_apps = data['app_path'].unique()
 
@@ -22,4 +23,17 @@ unique_apps_df = pd.DataFrame(unique_apps, columns=['app_path'])
 # Output Path
 output_file_path = os.path.join(cur_dir, 'unique_apps.csv')
 
+# Save the unique app paths to a CSV file
 unique_apps_df.to_csv(output_file_path, index=False)
+
+
+# * Create a CSV file with App Ratings
+# Generate app ratings based on how frequently apps are used
+app_ratings = data['app_path'].value_counts().reset_index()
+app_ratings.columns = ['app_path', 'rating']
+
+# Output Path
+output_file_path = os.path.join(cur_dir, 'app_ratings.csv')
+
+# Save the app ratings to a CSV file
+app_ratings.to_csv(output_file_path, index=False)
