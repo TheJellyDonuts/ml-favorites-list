47381eeb08961031ee309d8168427c43b1df6cdb
kcouv <kcouvion@cedarville.edu>
2024-03-07 22:03:19 -0700

    Decision_tree is more interpretable, and prints as pdf
    Co-authored-by: luke2119605349 <luke2119605349@users.noreply.github.com>

--- decision_tree.py
+++ decision_tree.py
@@ -3,27 +3,43 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
+import csv
 
 # iris example
+#%%
 iris = load_iris()
 X, y = iris.data, iris.target
 
 # import and sanitize csv data
-df = pd.read_csv('test_data/kardia-obfuserdata.csv', header=0)
+df = pd.read_csv('kardia_data_five_years.csv', header=0)[:100]
 df.drop_duplicates(inplace = True)
+file = open('src/data_generation/data/user_ids.csv', 'r')
+f_csv = csv.reader(file)
+next(f_csv)
+
+uids = dict(f_csv)
+user_ids = dict()
+for key, value in uids.items():
+    user_ids.update({value : int(key)})
 
 # format data
 dates = df.access_date.to_list()
 for i in range(len(dates)):
-    dates[i] = datetime.datetime.strptime(dates[i], '%Y-%m-%d %H:%M:%S').timestamp()
+    timestamp = datetime.datetime.strptime(dates[i], '%Y-%m-%d %H:%M:%S').time()
+    hour, minute, second = timestamp.hour, timestamp.minute, timestamp.second
+    dates[i] = (hour * 3600) + (minute * 60) + second
 users = df.user_name.to_list()
 # TODO: make user ids actually the right ones and not arbitrary
 for i in range(len(users)):
-    users[i] = i
+    users[i] = user_ids.get(users[i])
 X = [list(a) for a in zip(dates, users)]
 y = df.app_path.to_list()
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(X,y)
-tree.plot_tree(clf)
+tree.plot_tree(clf, node_ids=True, rounded=True)
+# plt.savefig('myimg.pdf', format="pdf")
+plt.figure()
 plt.show()
+
+# %%
