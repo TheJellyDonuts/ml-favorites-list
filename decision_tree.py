a784c315cf46d17cd6f9524dfe64373cf5347032
kcouv <kcouvion@cedarville.edu>
2024-03-08 11:32:25 -0700

    decision tree displays pretty for 100 samples

--- decision_tree.py
+++ decision_tree.py
@@ -1,45 +1,66 @@
-from sklearn.datasets import load_iris
 from sklearn import tree
 import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 import csv
 
-# iris example
-#%%
-iris = load_iris()
-X, y = iris.data, iris.target
-
-# import and sanitize csv data
+# import app access ta
 df = pd.read_csv('kardia_data_five_years.csv', header=0)[:100]
 df.drop_duplicates(inplace = True)
-file = open('src/data_generation/data/user_ids.csv', 'r')
-f_csv = csv.reader(file)
-next(f_csv)
 
-uids = dict(f_csv)
-user_ids = dict()
-for key, value in uids.items():
-    user_ids.update({value : int(key)})
+# import user ids
+user_file = open('src/data_generation/data/user_ids.csv', 'r')
+user_file_csv = csv.reader(user_file)
+next(user_file_csv)
 
-# format data
+# import app ids
+app_file = open('src/data_generation/data/app_ids.csv', 'r')
+app_file_csv = csv.reader(app_file)
+next(app_file_csv)
+
+# make dictionary to translate user_names to ids
+ids_to_users = dict(user_file_csv)
+users_to_ids = dict()
+for key, value in ids_to_users.items():
+    users_to_ids.update({value : int(key)})
+
+# make ascending-ordered list of app_paths
+ids_to_apps = dict(app_file_csv)
+apps = []
+for app in ids_to_apps.values():
+    apps.append(app)
+
+# format dates
 dates = df.access_date.to_list()
 for i in range(len(dates)):
     timestamp = datetime.datetime.strptime(dates[i], '%Y-%m-%d %H:%M:%S').time()
     hour, minute, second = timestamp.hour, timestamp.minute, timestamp.second
     dates[i] = (hour * 3600) + (minute * 60) + second
+
+# format users
 users = df.user_name.to_list()
-# TODO: make user ids actually the right ones and not arbitrary
 for i in range(len(users)):
-    users[i] = user_ids.get(users[i])
+    users[i] = users_to_ids.get(users[i])
+
+# prepare dates and users to pass into decision tree
 X = [list(a) for a in zip(dates, users)]
 y = df.app_path.to_list()
 
+# generate decision tree
 clf = tree.DecisionTreeClassifier()
 clf.fit(X,y)
-tree.plot_tree(clf, node_ids=True, rounded=True)
-# plt.savefig('myimg.pdf', format="pdf")
-plt.figure()
-plt.show()
+tree.plot_tree(
+    clf, 
+    node_ids=True, 
+    rounded=True, 
+    feature_names=['Timestamp', 'User'],
+    class_names=apps,
+    filled=True,
+    fontsize=2
+)
 
-# %%
+# display decision tree
+plt.title('Kardia App Access Recommendation Training')
+fig = plt.gcf()
+fig.set_size_inches(32, 32)
+fig.savefig('decision_tree.pdf', format='pdf')
